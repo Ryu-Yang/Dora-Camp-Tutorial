a3af4e89fb5e2c8092a2352f2c7b53995f2eba20
Ryu-Yang <Ryu-Yang@qq.com>
2024-12-07 13:13:49 +0800

    update lab3

--- src/trans_cmd.py
+++ src/trans_cmd.py
@@ -1,7 +1,7 @@
 from dora import Node
 import pyarrow as pa
 from enum import Enum
-
+import time
 
 PID_X = 0.0004
 PID_Y = -0.0003
@@ -13,14 +13,15 @@ class Action(Enum):
     RIGHT   = ("arm right",     "movec", [0, 0.02, 0, 0, 0, 0, 0.1])
     UP      = ("arm up",        "movec", [0, 0, -0.02, 0, 0, 0, 0.1])
     DOWN    = ("arm down",      "movec", [0, 0, 0.02, 0, 0, 0, 0.1])
-    CLOSE   = ("claw close",    "claw", [0])
-    OPEN    = ("claw open",     "claw", [100])
+    # CLOSE   = ("claw close",    "claw", [0])
+    # OPEN    = ("claw open",     "claw", [100])
     SAVE    = ("save",          "save", [0])
     CLEAR   = ("clear",         "clear", [0])
     BEGIN   = ("begin",         "begin", [0])
     STOP    = ("stop",          "stop", [0])
     GOTO    = ("goto",          "goto", [0])
 
+claw_state = 1
 
 node = Node()
 
@@ -36,6 +37,20 @@ for event in node:
                 if action.value[0] in text:
                     node.send_output(action.value[1], pa.array(action.value[2]))
                     print(f"""recieved:{action.value[0]}""")
+
+            if "claw close" in text:
+                if claw_state == 1:
+                    claw_state = 0
+                    DOWN = ("arm down", "movec", )
+                    node.send_output("movec", pa.array([0, 0, 0.06, 0, 0, 0, 0.1]))
+                    time.sleep(2)
+                    node.send_output("claw", pa.array([0]))
+                    time.sleep(2)
+                    node.send_output("movec", pa.array([0, 0, -0.06, 0, 0, 0, 0.1]))
+                    time.sleep(1)
+            if "claw open" in text:
+                claw_state = 1
+                node.send_output("claw", pa.array([100]))
 
         if event["id"] == "error":
             [error_x, error_y] =  event["value"].tolist()
